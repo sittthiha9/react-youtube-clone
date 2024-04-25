fa67ca8eaa468c2b512b417ca69c7b8fb7d3327b
Sitt Thiha Naing <sixxxxthihanaing@gmail.com>
2024-04-25 22:44:12 +0700

    updated

--- src/components/watch-video-contents/WatchVideoContents.styles.ts
+++ src/components/watch-video-contents/WatchVideoContents.styles.ts
@@ -24,8 +24,68 @@ export const WatchVideoContainer = styled.div`
 `;
 
 export const VideoScreen = styled.div`
-    width: 100%;
-    max-height: 44rem;
-    border-radius: 1rem;
-    overflow: hidden;
-`
+  width: 100%;
+  max-height: 44rem;
+  border-radius: 1rem;
+  overflow: hidden;
+`;
+
+export const VideoDetail = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 1rem;
+
+  .videoScreenTitle {
+    font-size: 20px;
+    font-weight: bold;
+  }
+`;
+
+export const VideoDescription = styled.div`
+  width: 100%;
+  background-color: ${({ theme: { grey2 } }) => grey2};
+  padding: 1rem;
+  border-radius: 1rem;
+  margin-top: 1rem;
+  line-height: 1.5rem;
+`;
+
+export const VideoDetailsActions = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 1rem;
+`;
+
+export const VideoDetailsInfo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const UserAccount = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1rem;
+  gap: 0.2rem;
+
+  .name {
+    font-weight: 800;
+    font-size: 16px;
+  }
+
+  .subscribe {
+    font-size: 13px;
+    color: ${({ theme: { grey3 } }) => grey3};
+  }
+`;
+
+export const SubscribeButton = styled.div`
+    padding: 0.6rem 0.6rem;
+    border-radius: 200rem;
+    background-color: ${({theme: {text}}) => text};
+    color: ${({theme: {background}}) => background};
+    margin-left: 2rem;
+    font-size: 15px;
+    font-weight: bold;
+`;
--- src/components/watch-video-contents/WatchVideoContents.tsx
+++ src/components/watch-video-contents/WatchVideoContents.tsx
@@ -1,10 +1,14 @@
 import {
   MoreVideosContainer,
   StyledWatchVideoContents,
+  SubscribeButton,
+  UserAccount,
+  VideoDetail,
+  VideoDetailsActions,
+  VideoDetailsInfo,
   VideoScreen,
   WatchVideoContainer,
 } from "./WatchVideoContents.styles";
-import Categories from "../categories/Categories";
 import { useAppContext } from "../../context/App.context";
 import RegularVideoItem from "../regular-video-item/RegularVideoItem";
 import { useParams } from "react-router-dom";
@@ -12,9 +16,11 @@ import { useEffect } from "react";
 import { getTitle } from "../../utils/videos";
 import { LoadingBackdrop } from "../content/Content.styles";
 import ReactPlayer from "react-player";
+import { Text } from "../../utils/Text.styles";
+import { RegularVideoPic } from "../regular-video-item/RegularVideoItem.styles";
 
 const WatchVideoContents = () => {
-  const { videos, fetchVideo, videoToWatchData, isFetchingVideos } =
+  const { videos, fetchVideo, videoToWatchData, isFetchingVideos, text } =
     useAppContext();
   const { id } = useParams();
 
@@ -40,11 +46,30 @@ const WatchVideoContents = () => {
             controls={true}
             volume={1}
             muted={false}
-            style={{  width: "100%" }}
+            style={{ width: "100%" }}
             playing={true}
             url={videoToWatchData?.video_files[0].link}
           />
         </VideoScreen>
+        <VideoDetail>
+          <Text className="videoScreenTitle">
+            {getTitle(videoToWatchData?.url + "")}
+          </Text>
+          <VideoDetailsActions>
+            <VideoDetailsInfo>
+              <RegularVideoPic>
+                <img src={videoToWatchData?.image} alt="profile" />
+              </RegularVideoPic>
+              <UserAccount>
+                <Text className="name">{videoToWatchData?.user.name}</Text>
+                <Text className="subscribers">{videoToWatchData?.duration}k {text.subscribers}</Text>
+              </UserAccount>
+              <SubscribeButton>
+                {text.subscribe}
+              </SubscribeButton>
+            </VideoDetailsInfo>
+          </VideoDetailsActions>
+        </VideoDetail>
       </WatchVideoContainer>
       <MoreVideosContainer>
         {/* <Categories /> */}
